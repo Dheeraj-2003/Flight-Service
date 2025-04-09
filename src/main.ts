89be34dc145a49883b087f69e387796d1030f181
Dheeraj-2003 <dheerajsolanki203@gmail.com>
2025-04-09 15:15:40 +0530

    added custom error and response handlers

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,8 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ConfigService } from '@nestjs/config';
-import { Logger } from 'winston';
-import { WINSTON_MODULE_NEST_PROVIDER } from 'nest-winston';
+import { ResponseInterceptor } from './common/interceptors/response.interceptor';
+import { AllExceptionFilter } from './common/filters/exception.filter';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -13,6 +13,11 @@ async function bootstrap() {
   const configService = app.get(ConfigService);
   const port = configService.get<number>('PORT') || 3000;
 
+  //Filters
+  app.useGlobalFilters(new AllExceptionFilter())
+
+  //Interceptors
+  app.useGlobalInterceptors(new ResponseInterceptor())
   await app.listen(port);
   console.log(`App is running on PORT:${port}`);
 }
