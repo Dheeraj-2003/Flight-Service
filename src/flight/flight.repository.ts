077e63819d43b01a93c30b0aa3a9f5b346e16c9c
Dheeraj-2003 <dheerajsolanki203@gmail.com>
2025-04-16 00:42:40 +0530

    added updateSeats in a transaction

--- src/flight/flight.repository.ts
+++ src/flight/flight.repository.ts
@@ -1,4 +1,4 @@
-import { Between, Repository, UpdateResult } from "typeorm";
+import { Between, QueryRunner, Repository, UpdateResult } from "typeorm";
 import { Flight } from "./flight.entity";
 import { InjectRepository } from "@nestjs/typeorm";
 import { BaseRepository } from "src/common/base.repository";
@@ -24,22 +24,28 @@ export class FlightRepository extends BaseRepository<Flight> {
     }
 
     async updateRemainingSeats(id:number, seats: number, dec: boolean = true) : Promise<Flight | null>{
+        const queryRunner : QueryRunner = this.flightRepository.manager.connection.createQueryRunner();
+        await queryRunner.startTransaction();
         try {
-            this.flightRepository.query('SELECT * FROM flight where flight.id = $1 FOR UPDATE', [id])
+            await queryRunner.query('SELECT * FROM flight where flight.id = $1 FOR UPDATE', [id])
             let response;
             if(dec){
-                response = await this.flightRepository.createQueryBuilder('flight').update(Flight).set({totalSeats: () => `"totalSeats"-:seats` }).where("id=:id", {seats,id}).execute();
+                response = await queryRunner.manager.createQueryBuilder().update(Flight).set({totalSeats: () => `"totalSeats"-:seats` }).where("id=:id", {seats,id}).execute();
             }
             else {
-                response = await this.flightRepository.createQueryBuilder().update(Flight).set({totalSeats: () => `"totalSeats"+:seats` }).where("id=:id", {seats,id}).execute();
+                response = queryRunner.manager.createQueryBuilder().update(Flight).set({totalSeats: () => `"totalSeats"+:seats` }).where("id=:id", {seats,id}).execute();
             }
             if(response.affected==0){
                 throw new NotFoundException()
             }
+            await queryRunner.commitTransaction();
             return this.findOneById(id);
         } catch (error) {
+            await queryRunner.rollbackTransaction()
             console.log(error)
             throw new HttpException("Couldn't alter totalSeats",HttpStatus.INTERNAL_SERVER_ERROR)
+        } finally{
+            await queryRunner.release();
         }
     }
 }
